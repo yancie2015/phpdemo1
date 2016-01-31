3b2329ff83fc352a311b70295e2be6f15b795a12
yancie2015 <18210427950@qq.com>
2016-01-31 21:27:17 +0800

    phpdemo1/index.php
    phpdemo1/index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?PHP
+  $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	$arr1 = array();
+	$arr1[] = array("linkurl"=>.$url."/static_html/demo1.php","title"=>"static_html");
+
+	foreach($arr1 as $item){
+		?>
+		<a href="<?PHP echo $item['linkurl']; ?>" target="_blank"><?PHP echo $item['title']; ?></a>
+		<?PHP
+	}
+?>
